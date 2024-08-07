8d9284248bc370e8087db68916b10b6acb648f40
Sadiq <samedov.sadiq55@gmail.com>
2024-08-07 21:01:02 +0400

    Minor fixes

--- BlogFrontend/Program.cs
+++ BlogFrontend/Program.cs
@@ -11,15 +11,15 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped<AuthenticationStateProvider, JwtAuthenticationStateProvider>();
-builder.Services.AddAuthorizationCore(options =>  {});
+builder.Services.AddAuthorizationCore();
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:5259") });
 builder.Services.AddScoped<TopicService>();
 builder.Services.AddScoped<BlogService>();
 
 builder.Services.AddHttpClient("IdentityService", httpClient =>
 {
-    httpClient.BaseAddress = new Uri("http://localhost:5058/");
+    httpClient.BaseAddress = new Uri("http://localhost:5259");
 });
 builder.Services.AddBlazoredLocalStorageAsSingleton();
 
