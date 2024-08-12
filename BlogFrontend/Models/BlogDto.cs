e2cbb5f212eb1af51625b92865418c2c6c9cdea0
Sadiq <samedov.sadiq55@gmail.com>
2024-08-12 23:04:08 +0400

    Add Like and Commit without logics

---
+++ BlogFrontend/Models/BlogDto.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlogFrontend.Models;
+
+public class BlogDto
+{
+    public Guid Id { get; set; }
+    public string Title { get; set; }
+    public string Text { get; set; }
+    public string UserName { get; set; }
+    public string PictureUrl { get; set; } 
+    public DateTime CreationDate { get; set; }
+}
