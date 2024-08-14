ceff6aca0672275087c07e5ae130f5b2900a0346
Sadiq <samedov.sadiq55@gmail.com>
2024-08-14 23:43:10 +0400

    Fixes

--- BlogFrontend/Models/User.cs
+++ BlogFrontend/Models/User.cs
@@ -2,14 +2,22 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace BlogFrontend.Models;
 
 public class User
 {
-    [Required]
-    public string? Name { get; set; }
     public string? AvatarUrl { get; set; }
-    public IEnumerable<UserTopic>? Topics {set; get;}
+
+    public ICollection<UserTopic> Topics { get; set; } = new List<UserTopic>();
+
+    public string? AboutMe { get; set; }
+    [JsonIgnore]
+    public ICollection<Blog> Blogs { get; set; } = new List<Blog>();
+
+    public ICollection<Comment> Comments { get; set; } = new List<Comment>();
+
+    public ICollection<Like> Likes { get; set; } = new List<Like>();
 }
