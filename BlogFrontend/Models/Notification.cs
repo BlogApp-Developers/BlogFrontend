fe624caa7baba7118f791999e37bb3b29a3d8fcc
Sadiq <samedov.sadiq55@gmail.com>
2024-08-18 01:26:43 +0400

    Add Notification

---
+++ BlogFrontend/Models/Notification.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlogFrontend.Models;
+
+public class Notification
+{
+    [Key]
+    public Guid Id { get; set; }
+    public Guid UserId { get; set; }
+    public string? Message { get; set; }
+    public string? Type { get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public bool IsRead { get; set; } = false;
+}
