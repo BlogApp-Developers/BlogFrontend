b4ebad665f1a8896f78f3198e755c5e0c14d8df7
Sadiq <samedov.sadiq55@gmail.com>
2024-09-05 00:40:14 +0400

    Minor fixes

--- BlogFrontend/Models/LoginDto.cs
+++ BlogFrontend/Models/LoginDto.cs
@@ -8,8 +8,8 @@ namespace BlogFrontend.Models;
 
 public class LoginDto
 {
-    [Required]
-    public string? Name { get; set; }
+    // [Required]
+    // public string? Name { get; set; }
     [Required]
     public string? Email { get; set; }
 
